2e1236e63fbb91db764fabd3e9d77958fed26fb2
Abdullah Alqahtani <0xqahta0@gmail.com>
2023-09-16 22:18:15 +0300

    refactor: refactor: hide secert envs and use .env.example file & change the dockerfile port

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/qahta0/saudi-cert/config"
 	"github.com/qahta0/saudi-cert/db"
 	"github.com/qahta0/saudi-cert/jobs"
 	"github.com/qahta0/saudi-cert/routes"
@@ -11,9 +14,15 @@ import (
 
 func main() {
 	db.ConnectToMongoDB()
-	jobs.RunSaudiCertCrawler(50, 55)
+	jobs.RunSaudiCertCrawler(50, 51)
 	jobs.SaudiCertInserter()
 	app := fiber.New()
+	app.Use(logger.New())
+	app.Use(cors.New())
 	routes.SetupRoutes(app)
-	log.Fatal(app.Listen(":3000"))
+	app.Use(func(c *fiber.Ctx) error {
+		return c.SendStatus(404)
+	})
+	PORT := config.Config("PORT")
+	log.Fatal(app.Listen(":" + PORT))
 }
